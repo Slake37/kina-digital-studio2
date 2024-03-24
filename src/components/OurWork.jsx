1c15b3fe7fd044879ca135bcf65f2ed762a046c9
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-03-24 16:57:42 +0000

    third commit

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -14,64 +14,66 @@ const OurWork = () => {
       <h2 className="text-[#D7191C] font-semibold text-2xl md:text-5xl">
         Works of Innovation.
       </h2>
-      <div className="flex flex-col justify-center items-center gap-3 md:flex-row md:gap-5 mt-3">
-        <div ref={ref} className="hover:scale-105 transition-all">
-          <motion.div
-            className="relative  cursor-pointer"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ ease: "linear", duration: 0.2 }}
-          >
-            <Image
-              src={taxi}
-              width={1250}
-              height={1250}
-              alt="Taxi"
-              className="w-full object-fill"
-            />
-            <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
-              Arima Hire
-            </p>
-          </motion.div>
-        </div>
-        <div ref={ref} className="hover:scale-105 transition-all">
-          <motion.div
-            className="relative"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ ease: "linear", duration: 0.2, delay: 0.3 }}
-          >
-            <Image
-              src={drivingSchool}
-              width={1250}
-              height={1250}
-              alt="Driving
+      <div ref={ref}>
+        <div className="flex flex-col justify-center items-center gap-3 md:flex-row md:gap-5 mt-3">
+          <div className="hover:scale-105 transition-all">
+            <motion.div
+              className="relative  cursor-pointer"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ ease: "linear", duration: 0.2 }}
+            >
+              <Image
+                src={taxi}
+                width={1250}
+                height={1250}
+                alt="Taxi"
+                className="w-full object-fill"
+              />
+              <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
+                Arima Hire
+              </p>
+            </motion.div>
+          </div>
+          <div className="hover:scale-105 transition-all">
+            <motion.div
+              className="relative"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ ease: "linear", duration: 0.2 }}
+            >
+              <Image
+                src={drivingSchool}
+                width={1250}
+                height={1250}
+                alt="Driving
               school"
-              className="w-full object-fill"
-            />
-            <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
-              Pride Driving School
-            </p>
-          </motion.div>
-        </div>
-        <div ref={ref} className="hover:scale-105 transition-all">
-          <motion.div
-            className="relative"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ ease: "linear", duration: 0.5, delay: 0.5 }}
-          >
-            <Image
-              src={maintenance}
-              width={1250}
-              height={1250}
-              alt="Maintenance"
-              className="w-full object-fill"
-            />
-            <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
-              Maintenancedotro
-            </p>
-          </motion.div>
+                className="w-full object-fill"
+              />
+              <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
+                Pride Driving School
+              </p>
+            </motion.div>
+          </div>
+          <div className="hover:scale-105 transition-all">
+            <motion.div
+              className="relative"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ ease: "linear", duration: 0.2 }}
+            >
+              <Image
+                src={maintenance}
+                width={1250}
+                height={1250}
+                alt="Maintenance"
+                className="w-full object-fill"
+              />
+              <p className="absolute bottom-0 left-0 bg-[#FAF9F6]/50 w-full text-[#0B1215] font-semibold text-lg px-2">
+                Maintenancedotro
+              </p>
+            </motion.div>
+          </div>
         </div>
       </div>
     </div>
