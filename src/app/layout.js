a27b69aac5e29976300fb2cc4bd921490ef8329d
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-03-26 20:47:13 +0000

    lol

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,8 +14,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="icon" href="./icons/favicon.ico" />
-        <meta property="og:title" content="Kina Digital Studio"></meta>
-        <meta property="og:image" content="https://ibb.co/RT5YsLw"></meta>
+        <meta property="og:title" content="Kina Digital Studio" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/B21cQ8M/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png"
+        />
+        <meta
+          property="og:description"
+          content="Web design and web development agency based in Newcastle-upon-Tyne, United Kingdom"
+        />
+        <meta property="og:type" content="article" />
       </Head>
       <body className="bg-[#0B1215] font-Roboto">
         <link rel="icon" href="/favicon-32x32.png" sizes="any" />
