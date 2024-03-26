18d4c4add50d88e7c8bb761c36f44ff51ac1a6ba
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-03-26 20:41:40 +0000

    some meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,24 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export const metadata = {
   title: "Kina Digital Studio",
   description: "Web design and web development Newcastle-upon-Tyne",
+  icons: { icon: "/icons/favicon-16x16.png" },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="./icons/favicon.ico" />
+        <meta property="og:title" content="Kina Digital Studio"></meta>
+        <meta property="og:image" content="https://ibb.co/RT5YsLw"></meta>
+      </Head>
       <body className="bg-[#0B1215] font-Roboto">
+        <link rel="icon" href="/favicon-32x32.png" sizes="any" />
         <Navbar />
         {children}
         <Footer />
