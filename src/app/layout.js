d13802d79739f8a619b7d268ac58ea5c8b367b54
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-03-30 09:33:54 +0000

    twitter, and facebook meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,56 @@ import image from "../../thumbnail.png";
 import Image from "next/image";
 
 export const metadata = {
-  title: "Kina Digital Studio",
-  description: "Web design and web development Newcastle-upon-Tyne",
+  title:
+    "Kina Digital Studio | Newcastle-upon-Tyne | Web design and Web development",
+  description:
+    "Web design and web development from Newcastle-upon-Tyne, United Kingdom",
   icon: "/favicon",
-  ogImage:
-    "https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png",
-  ogTitle: "Kina Digital Studio",
-  keywords: "Web design, Web development,Newcastle-upon-Tyne",
+  // ogImage:
+  //   "https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png",
+  // ogTitle: "Kina Digital Studio",
+  keywords: "Web design, Web development,Newcastle-upon-Tyne, United Kingdom",
   googleVerification: "icqMhhKujOX7gusIln6Rux14XdGhMWGvHuVvprmToa4",
+  openGraph: {
+    title: "Kina Digital Studio",
+    description:
+      "Web design and web development from Newcastle-upon-Tyne, United Kingdom",
+    url: "https://kinadigitalstudio.com",
+    siteName: "Kina Digital Studio",
+    images: [
+      {
+        url: "https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "Kina Digital Studio Thumbnail",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.png",
+  },
+  verification: {
+    google: "icqMhhKujOX7gusIln6Rux14XdGhMWGvHuVvprmToa4",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kina Digital Studio",
+    description:
+      "Web design and web development from Newcastle-upon-Tyne, United Kingdom",
+    creator: "Kina Digital Studio",
+
+    image:
+      "https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png",
+    // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -23,24 +65,36 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta
           name="google-site-verification"
-          content="icqMhhKujOX7gusIln6Rux14XdGhMWGvHuVvprmToa4"
+          content={metadata.verification.google}
         />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <meta property="og:title" content={metadata.ogTitle} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image:url" content={metadata.ogImage} />
+        <link rel="icon" href={metadata.icons.icon} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta
+          property="og:image:url"
+          content="https://i.ibb.co/sH2080c/Dark-Grey-Minimalist-Photo-Travel-You-Tube-Thumbnail.png"
+        />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kinadigitalstudio.com/" />
 
         <link rel="manifest" href="/site.webmanifest"></link>
-        <meta name="twitter:title" content={metadata.ogTitle} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.ogImage} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content={metadata.twitter.card} />
 
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta
+          name="twitter:description"
+          content={metadata.twitter.description}
+        />
+        <meta name="twitter:image" content={metadata.twitter.image} />
         <meta
           property="og:description"
           content="Web design and web development agency based in Newcastle-upon-Tyne, United Kingdom"
