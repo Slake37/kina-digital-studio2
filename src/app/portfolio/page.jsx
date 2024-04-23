04cfaa59b8836fbaf4b14944b85409ede2b2f4a8
Slake37 <moldovan.alin160@yahoo.co.uk>
2024-04-23 21:27:37 +0100

    shopping list added to portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,6 @@
 import taxi from "../../assets/taxi.jpg";
 import drivingSchool from "../../assets/drivingSchool.jpg";
-import maintenance from "../../assets/maintenance.jpg";
+import shopping from "../../assets/shopping.jpg";
 import movies from "../../assets/movies.jpg";
 import Image from "next/image";
 import Link from "next/link";
@@ -42,22 +42,7 @@ const Portfolio = () => {
             Pride Driving School
           </p>
         </Link>
-        <Link
-          href={"https://www.maintenancedotro.co.uk"}
-          target="_blank"
-          className="relative"
-        >
-          <Image
-            src={maintenance}
-            height={2250}
-            width={2250}
-            alt="Driving School"
-            className="rounded-md"
-          />
-          <p className="absolute bottom-0 w-full bg-[#FAF9F6]/50 text-xl p-2">
-            Maintenance.ro
-          </p>
-        </Link>
+
         <Link
           href={"https://moviezzz-swart.vercel.app/"}
           target="_blank"
@@ -72,6 +57,22 @@ const Portfolio = () => {
           />
           <p className="absolute bottom-0 w-full bg-[#FAF9F6]/50 text-xl p-2">
             Movies Database
+          </p>
+        </Link>
+        <Link
+          href={"https://myshoppinglist-six.vercel.app/"}
+          target="_blank"
+          className="relative"
+        >
+          <Image
+            src={shopping}
+            height={2250}
+            width={2250}
+            alt="Shopping list"
+            className="rounded-md"
+          />
+          <p className="absolute bottom-0 w-full bg-[#FAF9F6]/50 text-xl p-2">
+            Shopping list web application
           </p>
         </Link>
       </div>
